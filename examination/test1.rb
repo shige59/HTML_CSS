cd3abf4bd7f3c175c7976a444e3f505fdf07eb7c
shige59 <141386004+shige59@users.noreply.github.com>
2023-09-10 09:28:29 +0900

    initial commit

---
+++ examination/test1.rb
@@ -0,0 +1,13 @@
+class Student
+  def initialize(last_name, first_name)
+    @last_name = last_name
+    @first_name = first_name
+  end
+
+  def introduce
+    puts "私の名前は「#{@last_name + " " + @first_name}」です。"
+  end
+end
+
+student = Student.new("山田", "太郎")
+student.introduce
---
+++ examination/test2.rb
@@ -0,0 +1,6 @@
+def hello(hoge)
+  puts "こんにちは#{hoge}さん"
+end
+
+name = "鈴木"
+hello(name)
